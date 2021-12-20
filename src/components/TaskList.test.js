f880545e268036a38392b320d399cd3138d7cccf
svettorelo <sabrinavettorelo@gmail.com>
2021-12-20 12:07:47 -0300

    tasklist done and tested

---
+++ src/components/TaskList.test.js
@@ -0,0 +1,12 @@
+import {render} from "@testing-library/react";
+import {composeStories} from "@storybook/testing-react";
+import * as TaskListStories from "./TaskList.stories";
+
+const {Pinned} = composeStories(TaskListStories);
+it('Renders pinned tasks at the beginning of the list', () => {
+  const {container} = render(<Pinned/>);
+  expect(container.querySelector('.list-item:nth-child(1) input[value="Task 1 (pinned)"]')
+  ).not.toBe(null);
+  expect(container.querySelector('.list-item:nth-child(2) input[value="Task 6 (pinned)"]')
+  ).not.toBe(null);
+});
